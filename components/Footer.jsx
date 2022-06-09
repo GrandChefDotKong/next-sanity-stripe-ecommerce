c1b1c8e83f394cb6ade7c5b846e2f377aee12745
Adrien Pauchet <grandchefdotkong@gmail.com>
2022-06-09 15:59:14 +0900

    updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 @copyright</p>
+      <p>2022 copyright@adrien-pauchet</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
